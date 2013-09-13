147a0505b6d00ad4a9848d049b2c4a4f02597df2
Michael Aidane <maidane@gmail.com>
2013-09-13 12:13:12 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vrmanager::Application.config.secret_key_base = 'e51392e045354f86d2863a14c6d5c8a6ea7bcf97aef8b783126d1e5fb745b18cd9ecd025b5a7b0d496894200dce8a3369affe3bd47331ec5c752554d89831eb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vrmanager::Application.config.session_store :cookie_store, key: '_vrmanager_session'
